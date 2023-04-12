4900bf28fedc969993e56ae6afbb1a04f7df3a82
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-04-12 08:59:22 -0300

    Aprendendo a usar o create server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const http = require("http")
 
 http
-    .createServer((request, response) => {})
+    .createServer((request, response) => {
+        response.writeHead(200, { 'Content-Type': 'application/json' })
+        response.end(JSON.stringify({
+            message:'Conhecendo o NodeJS'
+        }))
+    })
     .listen(4001, () => console.log('servidor rodando na porta 4001'))
