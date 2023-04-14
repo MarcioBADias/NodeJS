04d6329b23ddd5af07e43ab2df51b1f434482229
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-04-14 09:52:05 -0300

    primeira rota

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.get()
+app.get('/', (request, response)=>{
+    return response.json({
+        message: 'acesso à primeira rota'
+    })
+})
 
 app.listen(4002, ()=> console.log('server 4002 on!'))
