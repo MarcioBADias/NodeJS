45677b61efa96ccd2f492ddafe41a816ea48b16a
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-04-14 10:49:26 -0300

    COmeçando a API Products

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response)=>{
-    return response.json({
-        message: 'acesso à primeira rota'
-    })
+app.use(express.json())
+
+const products = []
+
+/*
+*POST => Inserir um dado
+*GET => Buscar um ou mais dado dados
+*PUT => Alterar um dado
+*DELETE => Remover um dado
+*/
+
+/*
+*BODY => Sempre que eu quiser enviar dados para minha aplicação
+*PARAMS => /product/251547511154 (ID) 
+*QUERY => /product?id=26586268586562658&value=3525686846813
+*/
+
+app.post('/products', (request, response) => {
+
+    const body = request.body
+    console.log(body)
 })
 
 app.listen(4002, ()=> console.log('server 4002 on!'))
